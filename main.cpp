ec750d884447adbd407bf8572149f78276cbb59f
Noah Lindner <nlindner@carthage.edu>
2020-02-25 10:34:41 -0600

    added square() function
    This takes in an int and squares it.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 
 using namespace std;
 
+void square(int num){
+    int result = num * num;
+    cout << result << endl;
+}
+
 int main(){
 
     cout << "GitHub Test" << endl;
-
+    square(5);
     return 0;
 
 }
